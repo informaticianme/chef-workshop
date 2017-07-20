4ea884fddabdbf8e497872ce39e4f82c75b0a286
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2017-07-20 19:18:51 -0400

    Made changes for self-registering node

--- bootstrap-node.sh
+++ bootstrap-node.sh
@@ -17,19 +17,20 @@ sudo chown -R $USERNAME:$USERNAME /var/chef
 sudo chown -R $USERNAME:$USERNAME /var/lib/chef
 sudo chown -R $USERNAME:$USERNAME /var/log/chef
 
-# Generate node name
-NODENAME="$MACHINE_NAME-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)"
-
 # Create client.rb
 if [ -f /etc/chef/client.rb ]; then
 	dt=`date '+%Y%m%d-%H%M%S'`
 	mv /etc/chef/client.rb /etc/chef/client-$dt.rb
 fi
 
+# Generate node name
+NODENAME="$MACHINE_NAME-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)"
+
 cat <<EOT >> /etc/chef/client.rb
 log_level	:info
 log_location	"/var/log/chef/chef.log"
 chef_server_url	'https://chef.vmhost.psu.test/organizations/$SHORTNAME'
+node_name	'$NODENAME'
 validation_client_name '$SHORTNAME-validator'
 validation_key '/etc/chef/$SHORTNAME-validator.pem'
 ssl_verify_mode :verify_none
